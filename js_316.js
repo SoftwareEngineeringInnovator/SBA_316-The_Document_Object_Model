1397c497ea82d7f85e25ff307e3ff7019db105f1
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-05-30 15:50:18 -0400

    complete the selectElementById, querySelector and querySelectorAll, tested console.log()

--- js_316.js
+++ js_316.js
@@ -1,5 +1,46 @@
 // Test connection from project to web page
 // console.log("script connected");
 
-// Create variables using (const and let) for the JS and start testing their functionality
+// Cache at least one element using selectElementById. 5%. - IDs ("incident-form", "description", "char-counter", "incident-count",  "log-feed" )
+
+const incidentForm = document.getElementById("incident-form");
+
+const descriptionField = document.getElementById("description");
+
+const characterCounter = document.getElementById("char-counter");
+
+const incidentCount = document.getElementById("incident-count");
+
+const logFeed = document.getElementById("log-feed");
+
+
+// Cache at least one element using querySelector or querySelectorAll. 5% - class (".form-panel", ".log-panel", ".filter-btn", ".severity-badge")
+
+const formPanel = document.querySelector(".form-panel");
+
+const logPanel = document.querySelector(".log-panel");
+
+const filterButtons = document.querySelectorAll(".filter-btn");
+
+const severityBadges = document.querySelectorAll(".severity-badge");
+
+// Tested everyone of the above varialbles
+// console.log(severityBadges);
+
+
+// Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.). 5% 
+// Iterate over a collection of elements to accomplish some task. 10% 
+// Create at least one element using createElement. 5% 
+// Use appendChild and/or prepend to add new elements to the DOM. 5% 
+// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 2% 
+// Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent. 10% 
+// Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties. 5% 
+// Modify at least one attribute of an element in response to user interaction. 3% 
+// Register at least two different event listeners and create the associated event handler functions. 10% 
+// Use at least two Browser Object Model (BOM) properties or methods. 3% Include at least one form and/or input with HTML attribute validation. 5% 
+// Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.) 5% 
+// Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit). 10% 
+// Commit frequently to the git repository. 5% 
+// Include a README file that contains a description of your application. 2% 
+// Level of effort displayed in creativity, presentation, and user experience. 5%
 
